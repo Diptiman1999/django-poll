8fd8c893a614f343ec08eb08f58c150ea1e7cc38
Diptiman1999 <diptiman.sen1999@gmail.com>
2020-07-27 22:07:09 +0530

    Full Completion

--- poll/admin.py
+++ poll/admin.py
@@ -1,6 +1,30 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question,Choice
 # Register your models here.
 
-admin.site.register(Question)
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ['pub_Date', 'question_text']
+#This will make the publication date to come first followed by question_text
+
+class ChoiceInline(admin.StackedInline):#Stacked Line tells it will one after another
+# With that TabularInline (instead of StackedInline), the related objects are displayed in a more compact, table-based format
+#it will have an extra delte columns
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_Date'], 'classes': ['collapse']}),#Collapse is used thide it or show it
+    ]
+    inlines = [ChoiceInline]
+    #Here fieldsets is list containing tuples where first vale of the tuple represents  Heaing followed by fielsd which is a dictionary
+    # fieldsets , fields and inline are predifined words
+    list_display = ('question_text', 'pub_Date', 'was_published_recently')#Used to show the question in list view
+    list_filter = ['pub_Date']#Because pub_date is a DateTimeField, Django knows to give appropriate filter options: “Any date”, “Today”, “Past 7 days”, “This month”, “This year”
+    
+
+
+admin.site.register(Question, QuestionAdmin)
--- poll/models.py
+++ poll/models.py
@@ -7,12 +7,18 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_Date = models.DateTimeField('date published')
 
-    def __str__(self):
-        return self.question_text
+    # def __str__(self):#This unction is use to show the name of the question in the admin page
+    #     return self.question_text
 
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_Date <= now
+    was_published_recently.admin_order_field = 'pub_Date'           #It is used to short by date
+    was_published_recently.boolean = True                           #Wrong or write
+    was_published_recently.short_description = 'Published recently?'#Heading
+    
+
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
